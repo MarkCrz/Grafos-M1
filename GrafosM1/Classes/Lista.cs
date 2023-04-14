cd16173291a4c71edde2a8164f8a8bd149c8a84c
Bruno Pereira <bruxeetos@gmail.com>
2023-04-13 21:58:04 -0300

    a

--- GrafosM1/Classes/Lista.cs
+++ GrafosM1/Classes/Lista.cs
@@ -8,27 +8,27 @@ public class Lista : Grafo
         ponderado = p;
         vertices = new List<Vertice>();
     }
-    
+
     public bool inserirVertice(string label)
     {
         return InserirVerticeP(label);
     }
-    
+
     public string labelVertice(int indice)
     {
         return labelVerticeP(indice);
     }
-    
+
     public bool inserirAresta(int o, int d, float p)
     {
         return inserirArestaP(o, d, p);
     }
-    
+
     public void imprimeAresta(int indexVertice, int indexAresta)
     {
         imprimeArestaP(indexVertice, indexAresta);
     }
-    
+
     public void imprimeGrafo()
     {
         for (var i = 0; i < vertices.Count; i++)
@@ -38,11 +38,10 @@ public class Lista : Grafo
             Console.Write("\n");
         }
     }
-    
+
     public bool existeAresta(int origem, int destino)
     {
         return existeArestaP(origem, destino);
     }
-    
-    
+
 }
--- GrafosM1/Program.cs
+++ GrafosM1/Program.cs
@@ -1,6 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using GrafosM1.Classes;
+/*Primeira linha, salvar o número de v, a, se é d, e pon*/
+    /* Lê um arquivo linha a linha */
+
+    void criarPorListaTxt(string caminho)
+    {
+        var lines = File.ReadAllLines(caminho);
+        var primeiraLinha = lines[0];
+
+        //Console.WriteLine(primeiraLinha);
+        string[] listaDados = primeiraLinha.Split(" ");
+
+        if (listaDados[2] == "0" || listaDados[2] == "00") {
+            if (listaDados[3] == "0" || listaDados[3] == "00") {
+                //lista = new Lista(false, false);
+            } else {
+                //lista = new Lista(false, true);
+            }
+        } else {
+            if (listaDados[3] == "0" || listaDados[3] == "00") {
+                //lista = new Lista(true, false);
+            } else {
+                //lista = new Lista(true, true);
+            }
+        }
+
+        foreach (var line in lines)
+        {
+            string[] dados = line.Split(" ");
+            foreach (var dado in dados)
+            {
+                Console.WriteLine(dado);
+            }
+
+        }
+    }
 
     Matriz matriz = new Matriz(true, false);
     matriz.inserirVertice("Teste");
@@ -25,7 +60,8 @@ using GrafosM1.Classes;
     
     matriz.imprimeGrafo();
     //primeiro input para direcionada e segundo para ponderada
-    Lista lista = new Lista(false, false); 
+    Lista lista = new Lista(false, false);
+    
     lista.inserirVertice("Teste");
     lista.inserirVertice("Teste 2");
     lista.inserirVertice("Teste 3");
@@ -47,4 +83,9 @@ using GrafosM1.Classes;
     Console.Write("Remover aresta: " + lista.removerAresta(0, 2) + "\n");
     Console.Write("Remover vertice: " + lista.removerVertice("Teste") + "\n"); 
 
+
     lista.imprimeGrafo();
+
+    bool dir, pond;
+
+    criarPorListaTxt("C:\\Users\\Bruno\\OneDrive\\Documentos\\Grafos-M1\\GrafosM1\\teste.txt");
